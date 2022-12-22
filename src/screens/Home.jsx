f41c1f4f2acbb4170ea9a2fa61a0ac9160c9970b
Otor John Stephen <johnogenyi4real@gmail.com>
2022-12-22 13:45:23 +0100

    another edit

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -14,16 +14,15 @@ const Home = () => {
           <div className="hello">Hello, my name is</div>
           <div className="name">Otor John Stephen</div>
           <div className="description">
-            I am a <span>Creative and Enthusiastic UI UX Designer</span>
-            with over 3 years of hands-on experience in crafting user
-            experiences and design systems for software, web, and mobile
-            products. I am eager to contribute to team success through hard
-            work, attention to detail, and, excellent organizational skills. I
-            am motivated to learn, grow and excel in the design Industry. I have
-            an excellent experience in prototyping, UI design, UX design, User
-            research, Web design, and wireframing. I seek to deliver Ui-Ux
-            excellence for the betterment of any organization whose goal is to
-            attain excellence.
+            I am a <span>Creative and Enthusiastic UI UX Designer</span> with
+            over 3 years of hands-on experience in crafting user experiences and
+            design systems for software, web, and mobile products. I am eager to
+            contribute to team success through hard work, attention to detail,
+            and, excellent organizational skills. I am motivated to learn, grow
+            and excel in the design Industry. I have an excellent experience in
+            prototyping, UI design, UX design, User research, Web design, and
+            wireframing. I seek to deliver Ui-Ux excellence for the betterment
+            of any organization whose goal is to attain excellence.
           </div>
 
           <a
