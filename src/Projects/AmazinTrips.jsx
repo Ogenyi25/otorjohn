bf608d89763efb5a6183fbf0fc4650c7b9f57ed7
Otor John Stephen <johnogenyi4real@gmail.com>
2023-02-14 15:28:31 +0100

    updated the project section

---
+++ src/Projects/AmazinTrips.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import HEADER_IMAGE from "../asset/project/amazintripsbg.png";
+import BACK_ARROW from "../asset/backarrow.svg";
+
+const AmazinTrips = () => {
+  return (
+    <div className="amazintrips-section">
+      <div className="details">
+        <div className="goback">
+          <Link className="back" to="/projects">
+            {" "}
+            <img src={BACK_ARROW} alt="back-arrow" className="arrow" />
+          </Link>
+        </div>
+        <div className="header">
+          <img src={HEADER_IMAGE} alt="" className="header-image" />
+        </div>
+
+        <div className="body-content">
+          <div className="body-header">overview</div>
+          <div className="task">
+            <div className="head">Project Brief</div>
+            <div className="body">
+              Amazin Trips is a travel app (mobile, webapp, and website) that
+              will provide users the simplest experience of travel. In the app,
+              users can easily book their flight, hotel (accomodation), taxi
+              etc. In addition, users can subscribe to a plan of their choosing
+              that will help provide them with all the requirements for their
+              trip.
+            </div>
+          </div>
+          <div className="task">
+            <div className="head">The Task</div>
+            <div className="body">
+              The task was to design a mobile, and web app that will enable
+              users book a trip base off their choice, and also provide support
+              that will help provide them with all the requirements for their
+              trip. The design should include a website that will showcase a
+              home page, an about page that gives information about AmazinTrips,
+              what they do, their mission and vision as well as their core
+              values. It should also showcase a travel plan section that will
+              list out all the available travel plans, and also a contact page
+              that will give information as to how users can reach out to.
+            </div>
+          </div>
+          <div className="task">
+            <div className="head">The Team</div>
+            <div className="body">
+              I worked on this project as the ui-ux designer, amongst other team
+              mates which include a frontend web developer, backend web
+              developer, and a mobile app developer.
+            </div>
+          </div>
+          <div className="task">
+            <div className="head">Responsibility</div>
+            <div className="body">
+              As the ui-ux designer, I came up with the brand style and a High
+              fidelity design for the website - desktop and mobile view, the
+              mobile app, web app, and also an admin dashboard.
+            </div>
+          </div>
+          <div className="task">
+            <div className="head">Inspiration + Process</div>
+            <div className="body">
+              I was inspired by clean, colorful website designs that utilized
+              illustrated cards to express their software elements. I took
+              functionalities from clean sample mobile/web apps and converted
+              them into new clean cards and elements. I made use of shadow for a
+              clean look and effect. This was applied across all screens of the
+              entire website. I ran a competitive analysis of other similar
+              products like wakanow, check out features that they lacked and
+              leveraged on them to come up with a unique, user friendly design.
+            </div>
+          </div>
+          <div className="full-project">
+            <Link
+              to={{
+                pathname:
+                  "https://www.figma.com/file/pRwFiCqHzwcSrFKvoOAmVf/AmazinTrips?node-id=16%3A201&t=ts3nCYogUj87nS0a-1",
+              }}
+              target="_blank"
+              className="link"
+            >
+              {" "}
+              Click to see full project.{" "}
+            </Link>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AmazinTrips;
--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import INVESTIFY from "../asset/project/investify.png";
+import AMAZINTRIPS from "../asset/project/amazintrips.png";
 import OGALANDLORD from "../asset/project/ogalandlord/ogalandlord.png";
 // import EDUSWIFT from "../asset/project/Banner.png";
 import RETRORESUME from "../asset/project/retroresume.png";
@@ -22,6 +23,35 @@ const Projects = () => {
         <div className="projects">
           <div className="project-details">
             <div className="project-image">
+              <img src={AMAZINTRIPS} alt="" className="image" />
+            </div>
+            <div className="project-content">
+              <div className="head">Amazin Trips</div>
+              <div className="body">
+                Amazin Trips is a full-suite digital travel advisory platform
+                that supports travelers to manage their end-to-end travel needs.
+                Our multilevel approach to travel management isn't just centered
+                on bookings but extends to providing a full program range
+                support for a low-risk, efficient and affordable travel planning
+                experience.
+              </div>
+              <div className="tags">
+                <div className="tag">UI Design</div>
+                <div className="tag">Mobile App</div>
+                <div className="tag">Website</div>
+                <div className="tag">UX Design</div>
+                <div className="tag">Figma</div>
+                <div className="tag">WebApp</div>
+                <div className="tag">Travelling</div>
+              </div>
+              <Link className="link" to="/investaholic">
+                <div className="button">View project details</div>
+              </Link>
+            </div>
+          </div>
+
+          {/* <div className="project-details">
+            <div className="project-image">
               <img src={INVESTIFY} alt="" className="image" />
             </div>
             <div className="project-content">
@@ -42,8 +72,7 @@ const Projects = () => {
                 <div className="button">View project details</div>
               </Link>
             </div>
-          </div>
-
+          </div> */}
 
           <div className="project-details">
             <div className="project-image">
@@ -52,9 +81,11 @@ const Projects = () => {
             <div className="project-content">
               <div className="head">retroresume</div>
               <div className="body">
-                Retroresume is a resume builder application to help users crack the code for writing perfect resumes.
-                Plus, it gives you a state-of-the art design to create a beautiful, professional job application resume.
-                Whether this is your first job or you’re looking for a promotion, it will help you stand above the rest
+                Retroresume is a resume builder application to help users crack
+                the code for writing perfect resumes. Plus, it gives you a
+                state-of-the art design to create a beautiful, professional job
+                application resume. Whether this is your first job or you’re
+                looking for a promotion, it will help you stand above the rest
                 by highlighting your personality and achievements..
               </div>
               <div className="tags">
@@ -69,7 +100,6 @@ const Projects = () => {
               </Link>
             </div>
           </div>
-
         </div>
 
         <div className="projects">
@@ -80,8 +110,10 @@ const Projects = () => {
             <div className="project-content">
               <div className="head">Artinict</div>
               <div className="body">
-                This is the official website for Artinict - A tech base organization that provides Advanced and Innovative IT Solutions.
-                The website showcases the Home, About us, Services, Portfolio and Contact page.
+                This is the official website for Artinict - A tech base
+                organization that provides Advanced and Innovative IT Solutions.
+                The website showcases the Home, About us, Services, Portfolio
+                and Contact page.
               </div>
               <div className="tags">
                 <div className="tag">UI Design</div>
@@ -95,7 +127,6 @@ const Projects = () => {
             </div>
           </div>
 
-
           <div className="project-details neiti">
             <div className="project-image">
               <img src={OGALANDLORD} alt="" className="image" />
@@ -103,8 +134,10 @@ const Projects = () => {
             <div className="project-content">
               <div className="head">OGA-LANDLORD</div>
               <div className="body">
-                Oga Landlord is a mobile application to help people search for and rent apartments in any location of their choice.
-                This helps remove the stress of physically going to scout for houses, it also take away the cost of agent fees..
+                Oga Landlord is a mobile application to help people search for
+                and rent apartments in any location of their choice. This helps
+                remove the stress of physically going to scout for houses, it
+                also take away the cost of agent fees..
               </div>
               <div className="tags">
                 <div className="tag">UI Design</div>
@@ -123,21 +156,29 @@ const Projects = () => {
 
         <div className="title">other projects</div>
         <div className="explain">
-          I've also created and prototyped other designs for different imaginary products as a means of improving
-          on my design skills and putting into practice other design techniques and patterns.
-          Some of these designs include -:
+          I've also created and prototyped other designs for different imaginary
+          products as a means of improving on my design skills and putting into
+          practice other design techniques and patterns. Some of these designs
+          include -:
         </div>
         <div className="other-projects">
           <div className="project-cards">
             <img src={NUEMORPHIC} alt="" className="card" />
             <div className="project-name">Car Remote App</div>
             <div className="project-description">
-              This is a dark screen Nuemorphic mobile app design for a car brand (Benz-G63).
-              It showcases a lock screen, a home page with icon links to other subpages for temperature, battery life, location, etc.
+              This is a dark screen Nuemorphic mobile app design for a car brand
+              (Benz-G63). It showcases a lock screen, a home page with icon
+              links to other subpages for temperature, battery life, location,
+              etc.
             </div>
 
-            <div className="prototype">Click
-              <a href="https://www.figma.com/proto/eOYas8heyU5jXVXAwPE6Dm/February-Projects?node-id=265%3A7060&scaling=scale-down&page-id=264%3A6960&starting-point-node-id=279%3A8673" rel="noreferrer" target="_blank">
+            <div className="prototype">
+              Click
+              <a
+                href="https://www.figma.com/proto/eOYas8heyU5jXVXAwPE6Dm/February-Projects?node-id=265%3A7060&scaling=scale-down&page-id=264%3A6960&starting-point-node-id=279%3A8673"
+                rel="noreferrer"
+                target="_blank"
+              >
                 here
               </a>
               to see the prototype for this design.
@@ -148,35 +189,46 @@ const Projects = () => {
             <img src={CHOOPIES} alt="" className="card" />
             <div className="project-name">Choopies</div>
             <div className="project-description">
-              Choopies is a mobile food delivery app meant to help users (customers)
-              place order of their favorite snack and have it delivered in a short while.
-              It features order tracking, fast in-app payment and lots more.
+              Choopies is a mobile food delivery app meant to help users
+              (customers) place order of their favorite snack and have it
+              delivered in a short while. It features order tracking, fast
+              in-app payment and lots more.
             </div>
 
-            <div className="prototype">Click
-              <a href="https://www.figma.com/proto/eOYas8heyU5jXVXAwPE6Dm/February-Projects?node-id=37%3A76&scaling=scale-down&page-id=34%3A64&starting-point-node-id=64%3A615" rel="noreferrer" target="_blank">
+            <div className="prototype">
+              Click
+              <a
+                href="https://www.figma.com/proto/eOYas8heyU5jXVXAwPE6Dm/February-Projects?node-id=37%3A76&scaling=scale-down&page-id=34%3A64&starting-point-node-id=64%3A615"
+                rel="noreferrer"
+                target="_blank"
+              >
                 here
               </a>
               to see the prototype for this design.
             </div>
-
           </div>
 
           <div className="project-cards">
             <img src={SMART_HOME_APP} alt="" className="card" />
             <div className="project-name">Smart Home App</div>
             <div className="project-description">
-              The smart home app as the name implies is a smart home automation application designed in both light and dark theme.
-              It showcases the different rooms and the applicances in them that can be remotely controlled.
+              The smart home app as the name implies is a smart home automation
+              application designed in both light and dark theme. It showcases
+              the different rooms and the applicances in them that can be
+              remotely controlled.
             </div>
 
-            <div className="prototype">Click
-              <a href="https://www.figma.com/proto/eOYas8heyU5jXVXAwPE6Dm/February-Projects?node-id=396%3A5504&scaling=scale-down&page-id=326%3A9051&starting-point-node-id=396%3A5556" rel="noreferrer" target="_blank">
+            <div className="prototype">
+              Click
+              <a
+                href="https://www.figma.com/proto/eOYas8heyU5jXVXAwPE6Dm/February-Projects?node-id=396%3A5504&scaling=scale-down&page-id=326%3A9051&starting-point-node-id=396%3A5556"
+                rel="noreferrer"
+                target="_blank"
+              >
                 here
               </a>
               to see the prototype for this design.
             </div>
-
           </div>
         </div>
       </div>
